5e6de7cbea9da473ce570ee20d6f6aacec45e35b
Disego <disegorachelm@gmail.com>
2024-05-10 16:26:34 +0200

    user stories 1 to 3 solved

--- scripts.js
+++ scripts.js
@@ -5,5 +5,22 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
-  result.innerText = dividend / divider;
+  
+
+  if (isNaN(dividend) || isNaN(divider)) {
+    result.innerText = "Something critical went wrong. Please reload the page"
+    result.classList.add("critical-error")
+  } else if (dividend === '' || divider === '') {
+    result.innerText = "Division not performed. Both values are required in inputs. Try again";
+    result.classList.add("error-message")
+  }else if (divider === '0'){
+    result.innerText = "Division not performed. Invalid number provided. Try again."
+    result.classList.add("error-message")
+    console.error("Devision by 0 is undefined")
+  } else {
+    result.innerText = Math.floor(dividend / divider);
+    result.classList.remove("error-message")
+  }
 });
+
+
